d2a3063f3fd7f6d07619f0a477f5bc57aa8f9a8a
NaldyXploit-ID <riskyhayun21@gmail.com>
2025-10-11 08:16:51 +0000

    test: add basic unit test

--- Mass-Add-File-Name-tests/test_basic.py
+++ Mass-Add-File-Name-tests/test_basic.py
@@ -2,3 +2,4 @@ def test_generate():
     from main import generate, sample_schema
     data = generate(sample_schema(), count=2, seed=1)
     assert isinstance(data, list) and len(data) == 2
+2025-10-16 12:47:06 - test: add basic unit test
